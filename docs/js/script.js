daa950d7a798bd016b51fdf70e8f77abdfb72e85
dmitriy152 <romanovds33@yandex.ru>
2022-11-03 18:26:29 +0300

    Add your commit

---
+++ docs/js/script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  disableOnInteraction: true,
+  stopOnLastSlide: false,
+  loop: true,
+  clickable: true,
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+setInterval(() => {
+  swiper.slideNext();
+}, 5000);
